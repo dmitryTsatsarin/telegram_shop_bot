4cd52ff556b50fdc0ac121b027688696a90ce1f5
forward <dmitry.tsatsarin@gmail.com>
2017-03-03 00:33:49 +0300

    обновлен конфиг прода

--- telegram_shop_bot/settings/prod.py
+++ telegram_shop_bot/settings/prod.py
@@ -30,7 +30,7 @@ LOGGING = {
     'formatters': {
         'verbose': {
             'format': '%(levelname)s %(asctime)s %(module)s '
-                      '%(process)d %(thread)d %(message)s'
+                      '%(message)s'
         },
     },
     'handlers': {
