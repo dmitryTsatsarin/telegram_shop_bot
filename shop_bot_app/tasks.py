2574d9fb8a031b7c7db531591f4b2fb4f653a912
forward <dmitry.tsatsarin@gmail.com>
2017-03-03 01:18:19 +0300

    Исправлена celery таска

--- shop_bot_app/tasks.py
+++ shop_bot_app/tasks.py
@@ -2,33 +2,23 @@
 import logging
 
 import arrow
-import telebot
 from celery import shared_task
-from django.conf import settings
 
 from shop_bot_app.logic import send_schedule_product
-from shop_bot_app.models import PostponedPost, Customer, PostponedPostResult
-from shop_bot_app.utils import create_shop_telebot
+from shop_bot_app.models import PostponedPost, Buyer, PostponedPostResult
 
 logger = logging.getLogger(__name__)
-
-
 
 
 @shared_task
 def post_by_schedule():
 
-    #todo: доделать эту часть !!!!!!!!!!!!!!!!!!! сделать выборку для каких ботов есть отложенные посты и отправить
-
-
-    create_shop_telebot()
-
     postponed_posts = list(PostponedPost.objects.filter(send_at__lte=arrow.now().datetime))
-    customers = list(Customer.objects.filter())
+    buyers = list(Buyer.objects.filter())
     # todo: опимизировать все в один запрос (но пока несрочно)
-    for customer in customers:
+    for buyer in buyers:
         for postponed_post in postponed_posts:
-            if not PostponedPostResult.objects.filter(customer=customer, postponed_post=postponed_post).exists():
-                send_schedule_product(shop_telebot, customer.chat_id, postponed_post.product_id, postponed_post.description)
-                PostponedPostResult.objects.create(customer=customer, postponed_post=postponed_post, is_sent=True)
-                logger.info(u'Запущен PostponedPost с id=%s' % postponed_post.id)
+            if not PostponedPostResult.objects.filter(buyer=buyer, postponed_post=postponed_post).exists():
+                send_schedule_product(buyer.chat_id, postponed_post.product_id, postponed_post.description)
+                PostponedPostResult.objects.create(buyer=buyer, postponed_post=postponed_post, is_sent=True)
+                logger.info(u'Запущен PostponedPost с id=%s к buyer=(%s, %s)' % (postponed_post.id, buyer.id, buyer.full_name))
