509dd64157e42fbeaefc6e60bee561e360fbe4ee
forward <dmitry.tsatsarin@gmail.com>
2017-03-04 22:18:32 +0300

    Поправлена масска url так как выяснилось что символы в токене могут быть не только буквами, но и знаками("_","-")

--- shop_bot_app/urls.py
+++ shop_bot_app/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls import include, url
 from shop_bot_app.views import webhooks
 
 urlpatterns = [
-    url(r'^(?P<token>\w+:\w+)/$', webhooks),
+    url(r'^(?P<token>.+:.+)/$', webhooks),
 ]
