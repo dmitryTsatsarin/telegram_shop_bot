9848181b9c9057b9841cd8e7dd44da9e6444cd8e
forward <dmitry.tsatsarin@gmail.com>
2017-03-05 04:16:34 +0300

    поменял базу на локальную

--- telegram_shop_bot/settings/local.py
+++ telegram_shop_bot/settings/local.py
@@ -79,7 +79,7 @@ LOGGING = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.prod.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
     'stat': {
         'ENGINE': 'django.db.backends.sqlite3',
