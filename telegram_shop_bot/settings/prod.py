983505b65d060cfcc9dd68ed822c7486e2e8693c
forward <dmitry.tsatsarin@gmail.com>
2017-03-10 22:41:07 +0300

    Изменен конфиг прода

--- telegram_shop_bot/settings/prod.py
+++ telegram_shop_bot/settings/prod.py
@@ -15,8 +15,12 @@ DEBUG = False
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.prod.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shop_bot',
+        'USER': 'webrunner',
+        'PASSWORD': 'ZHkv8GD4Zab2g4HHNqPn',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
